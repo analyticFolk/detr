cd98687d683c670627dfa29424ca6e01de8767f4
Chul Gwon <csgwon@gmail.com>
2020-06-18 15:42:55 -0400

    implementation of diou and ciou

--- util/box_ops.py
+++ util/box_ops.py
@@ -4,7 +4,7 @@ Utilities for bounding box manipulation and GIoU.
 """
 import torch
 from torchvision.ops.boxes import box_area
-
+import math
 
 def box_cxcywh_to_xyxy(x):
     x_c, y_c, w, h = x.unbind(-1)
@@ -60,6 +60,62 @@ def generalized_box_iou(boxes1, boxes2):
 
     return iou - (area - union) / area
 
+
+def distance_box_iou(boxes1, boxes2):
+    """
+    Distance IoU from https://arxiv.org/pdf/1911.08287.pdf
+
+    The boxes should be in [x0, y0, x1, y1] format
+
+    Returns a [N, M] pairwise matrix, where N = len(boxes1)
+    and M = len(boxes2)
+    """
+    # degenerate boxes gives inf / nan results
+    # so do an early check
+    assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
+    assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
+    iou, union = box_iou(boxes1, boxes2)
+
+    lt = torch.min(boxes1[:, None, :2], boxes2[:, :2])
+    rb = torch.max(boxes1[:, None, 2:], boxes2[:, 2:])
+    wh = (rb - lt).clamp(min=0)  # [N,M,2]
+
+    centers1 = 0.5*(boxes1[:,2:]+boxes1[:,:2])
+    centers2 = 0.5*(boxes2[:,2:]+boxes2[:,:2])
+
+    center_dist_sq = torch.sum((centers1[:,None,:]-centers2)**2, axis=2)
+    diag_dist_sq = wh[:, :, 0] ** 2 + wh[:, :, 1] ** 2
+    norm_dist_sq = center_dist_sq/diag_dist_sq
+
+    return iou - norm_dist_sq
+
+def complete_box_iou(boxes1, boxes2):
+    """
+    Complete IoU from https://arxiv.org/pdf/1911.08287.pdf
+
+    The boxes should be in [x0, y0, x1, y1] format
+
+    Returns a [N, M] pairwise matrix, where N = len(boxes1)
+    and M = len(boxes2)
+    """
+    # degenerate boxes gives inf / nan results
+    # so do an early check
+    # explicity greater than so that width and height must be non-zero
+    assert (boxes1[:, 2:] > boxes1[:, :2]).all()
+    assert (boxes2[:, 2:] > boxes2[:, :2]).all()
+
+    wh1 = boxes1[:,2:]-boxes1[:,:2]
+    wh2 = boxes2[:,2:]-boxes2[:,:2]
+    atan1 = torch.atan(wh1[:, 0] / wh1[:, 1])
+    atan2 = torch.atan(wh2[:, 0] / wh2[:, 1])
+    v = 4.0/math.pi*(atan1[:,None]-atan2[None,:])**2.
+
+    iou, union = box_iou(boxes1, boxes2)
+    a = v/((1-iou)+v)
+
+    diou = distance_box_iou(boxes1, boxes2)
+
+    return diou - a*v
 
 def masks_to_boxes(masks):
     """Compute the bounding boxes around the provided masks
